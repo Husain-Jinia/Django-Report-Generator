2a55b1dfcafebcfd73d8b88cb128d196bbfb9525
Husain Jinia <husainjinia0201@gmail.com>
2021-05-14 20:23:34 +0530

    Creating, merging & grouping dataframe (commit 2)

--- src/sales/forms.py
+++ src/sales/forms.py
@@ -1,3 +1,12 @@
 from django import forms
 
-class SalesSearchForm(forms.Form)
+CHART_CHOICES = (
+    ('#1','Bar Chart'),
+    ('#2','Pie Chart'),
+    ('#3','Line Chart'),
+)
+
+class SalesSearchForm(forms.Form):
+    date_from = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    date_to = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    chart_type = forms.ChoiceField(choices=CHART_CHOICES)
